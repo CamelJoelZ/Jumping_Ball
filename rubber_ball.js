bd9599333faa826f6ec0cd2fd42128a58279c272
JoelWen <zhw281@ucsd.edu>
2018-08-04 16:01:15 +0800

    Rubber_Ball air resistance

--- rubber_ball.js
+++ rubber_ball.js
@@ -2,11 +2,12 @@ var hero;
 const INITPOS = 300;
 const LEFTBOUND = 0;
 const RIGHTBOUND = 600;
-const UPSPEED = -20;
-const GRAVITY = 1;
+const UPSPEED = -10;
+const GRAVITY = 0.5;
 const BOOST = 10;
-const XSPEED = 3;
-const GROUNDWIDTH = 12;
+const XSPEED = 3.5;
+const GROUNDWIDTH = 12.5;
+const AIRRESIS = 0.02;
 
 function setup() {
   createCanvas(RIGHTBOUND, 505);
@@ -18,8 +19,8 @@ function draw() {
   background(200);
   hero.bounce();
   hero.update();
-  if(hero.y <= INITPOS-4)
-  hero.show();
+  if(!keyIsPressed === true) hero.airResis();
+  if(hero.y <= INITPOS-4) hero.show();
   rect(0,INITPOS-8,RIGHTBOUND,GROUNDWIDTH);
 }
 
@@ -35,6 +36,7 @@ function keyPressed(){
   if(keyCode === DOWN_ARROW){
     // give it a downward boost
     hero.acc = BOOST;
+    // keep the cube on the ground if key not released
   }
 }
 
@@ -65,6 +67,22 @@ function dot(){
     if(this.x + this.dirX > RIGHTBOUND - 10 || this.x + this.dirX < LEFTBOUND) return;
     this.x += this.dirX;
   }
+  /*
+   * simulate air resistance applied on the object
+   */
+   this.airResis = function(){
+      if(this.dirX > 0) this.dirX -= AIRRESIS;
+      else if(this.dirX < 0) this.dirX += AIRRESIS;
+   }
+
+  /*
+   * keep the ball on the ground
+   */
+   //this.stay = function(){
+  //    if(this.y >= INITPOS && keyIsPressed === true){
+   //      this.y = INITPOS;
+   //    }
+   //}
 
   /*
    * simulate the gravity applied on the rubber ball
@@ -72,16 +90,18 @@ function dot(){
   this.bounce = function(){
 
     if(this.ySpeed +  0.001*this.acc <= INITPOS){
-      this.y += this.ySpeed + 0.0000000001 * this.acc;
+      this.y += this.ySpeed + 0.0000000000001 * this.acc;
       this.ySpeed = this.ySpeed + this.acc;
     }
     if(this.y >= INITPOS){
       // bounce back after a downward boost
-      if(this.acc == BOOST) this.ySpeed = UPSPEED * 1.3;
+      if(this.acc == BOOST) this.ySpeed = UPSPEED * 1.5;
       else this.ySpeed = UPSPEED;
       // set the acc back to gravity after hit the ground
       this.acc = GRAVITY;
     }
+    // check if DOWN_ARROW key is being pressed
+    //this.stay();
   }
 
 }
