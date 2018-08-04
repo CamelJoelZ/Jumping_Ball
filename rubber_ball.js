98fd3208f3bba67438d395e2571ffbbd3a3473b0
JoelWen <zhw281@ucsd.edu>
2018-08-04 15:29:23 +0800

    Rubber_Ball downward boost

--- rubber_ball.js
+++ rubber_ball.js
@@ -1,10 +1,12 @@
 var hero;
-const INITPOS = 250;
+const INITPOS = 300;
 const LEFTBOUND = 0;
-const RIGHTBOUND = 1005;
-const UPSPEED = -22;
-const GRAVITY = 1.5;
-const BOOST = 20;
+const RIGHTBOUND = 600;
+const UPSPEED = -20;
+const GRAVITY = 1;
+const BOOST = 10;
+const XSPEED = 3;
+const GROUNDWIDTH = 12;
 
 function setup() {
   createCanvas(RIGHTBOUND, 505);
@@ -16,17 +18,19 @@ function draw() {
   background(200);
   hero.bounce();
   hero.update();
+  if(hero.y <= INITPOS-4)
   hero.show();
+  rect(0,INITPOS-8,RIGHTBOUND,GROUNDWIDTH);
 }
 
 function keyPressed(){
   if(keyCode === RIGHT_ARROW){
     // moving to the right
-    hero.dirX = 5;
+    hero.dirX = XSPEED;
   }
   if(keyCode === LEFT_ARROW){
     // moving to the left
-    hero.dirX = -5;
+    hero.dirX = -XSPEED;
   }
   if(keyCode === DOWN_ARROW){
     // give it a downward boost
@@ -58,20 +62,25 @@ function dot(){
    * stop updating if bounds arrived
    */
   this.update = function(){
-    if(this.x + this.dirX > RIGHTBOUND - 10 || this.x + this.dirX < UPSPEED) return;
+    if(this.x + this.dirX > RIGHTBOUND - 10 || this.x + this.dirX < LEFTBOUND) return;
     this.x += this.dirX;
   }
 
   /*
-   * simulate the gravity applied on the rubber ball 
+   * simulate the gravity applied on the rubber ball
    */
   this.bounce = function(){
 
-    this.y += this.ySpeed + 0.01 * this.acc;
-    this.ySpeed = this.ySpeed + this.acc;
+    if(this.ySpeed +  0.001*this.acc <= INITPOS){
+      this.y += this.ySpeed + 0.0000000001 * this.acc;
+      this.ySpeed = this.ySpeed + this.acc;
+    }
     if(this.y >= INITPOS){
+      // bounce back after a downward boost
+      if(this.acc == BOOST) this.ySpeed = UPSPEED * 1.3;
+      else this.ySpeed = UPSPEED;
+      // set the acc back to gravity after hit the ground
       this.acc = GRAVITY;
-      this.ySpeed = UPSPEED;
     }
   }
 
